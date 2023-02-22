8b14fcf5685683730ad25f0ca7f1cdf04becec65
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2023-02-22 09:32:51 +0800

    update homepage

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -2,37 +2,34 @@ import React from "react";
 import Login from "./Login";
 import Signup from "./Signup";
 import "./landing.css";
+import { Link } from 'react-scroll';
+import Video from '../../assets/hero.mov';
 
 const Landing = () => {
   return (
     <>
-      <div className="container">
-        <div className={"parallax text-center1"}>
-          <img
-            draggable="false"
-            id="mountain"
-            src="https://www.design-fenix.com.ar/codepen/parallax/mountain.png"
-            alt=""
-          />
+        <div className="hero w-full  relative text-white">
+				<video
+					autoPlay
+					loop
+					muted
+					id="video"
+					className="w-full object-cover z-[8]"
+				>
+					<source src={Video} type="video/mp4" />
+				</video>
+				<div className="overlay bg-[#00000016]"></div>
+       
+				<div className="content w-full h-full m-auto p-4 absolute top-0 flex flex-col justify-center items-center">
+        <h1 className="text-center1 land-header">TRAVELER MATE</h1>
 
-          <img
-            draggable="false"
-            id="island"
-            src="https://www.design-fenix.com.ar/codepen/parallax/island.png"
-            alt=""
-          />
-          <img
-            draggable="false"
-            id="boat"
-            src="https://www.design-fenix.com.ar/codepen/parallax/boat.png"
-            alt=""
-          />
-          <h1 className="text-center1 land-header">TRAVELLER MATE</h1>
+        					<Link to="search" smooth={true} duration={500}>
           <div className="lowBtn">
             <Login /> <strong>- OR -</strong> <Signup />
-          </div>
+          </div>					</Link>
+				</div>
         </div>
-      </div>
+     
     </>
   );
 };
